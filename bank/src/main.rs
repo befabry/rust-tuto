9032438441d2fa9e45b5887bbed367c9b056cc80
befabry <fabrybenjamin5@gmail.com>
2025-10-08 23:25:21 +0200

    Second chapter: bank project

---
+++ bank/src/main.rs
@@ -0,0 +1,40 @@
+#[derive(Debug)]
+struct Account {
+    id: u32,
+    balance: u32,
+    holder: String,
+}
+
+impl Account {
+    fn new(id: u32, holder: String) -> Self {
+        Account {
+            id,
+            balance: 0,
+            holder,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Bank {
+    accounts: Vec<Account>,
+}
+
+impl Bank {
+    fn new() -> Self {
+        Bank { accounts: vec![] }
+    }
+}
+
+fn print_account(account: Account) {
+    println!("{:#?}", account);
+}
+
+fn main() {
+    let bank = Bank::new();
+    let account = Account::new(1, String::from("me"));
+
+    // println!("{:#?}", bank);
+    print_account(account);
+    print_account(account);
+}
